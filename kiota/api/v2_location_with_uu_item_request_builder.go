80ca6306c51c5e7fc171a2b0633f73b7da9e5637
Jesús García Crespo <jesus@sevein.com>
2024-05-13 19:48:35 +0000

    Fix move files API
    Describe the body request payload with MoveRequest model.

--- kiota/api/v2_location_with_uu_item_request_builder.go
+++ kiota/api/v2_location_with_uu_item_request_builder.go
@@ -57,45 +57,24 @@ func (m *V2LocationWithUuItemRequestBuilder) Get(ctx context.Context, requestCon
     return res.(i2b7a3625368152c59661ed1a63c26960f7e9cda05d0fbc8e5d79ac57ca250e0a.Locationable), nil
 }
 // Post move files to the specified location.
-// Deprecated: This method is obsolete. Use PostAsWithUuPostResponse instead.
-// returns a V2LocationItemWithUuResponseable when successful
+// returns a []byte when successful
 // returns a Error error when the service returns a 400 status code
-func (m *V2LocationWithUuItemRequestBuilder) Post(ctx context.Context, requestConfiguration *V2LocationWithUuItemRequestBuilderPostRequestConfiguration)(V2LocationItemWithUuResponseable, error) {
-    requestInfo, err := m.ToPostRequestInformation(ctx, requestConfiguration);
+func (m *V2LocationWithUuItemRequestBuilder) Post(ctx context.Context, body i2b7a3625368152c59661ed1a63c26960f7e9cda05d0fbc8e5d79ac57ca250e0a.MoveRequestable, requestConfiguration *V2LocationWithUuItemRequestBuilderPostRequestConfiguration)([]byte, error) {
+    requestInfo, err := m.ToPostRequestInformation(ctx, body, requestConfiguration);
     if err != nil {
         return nil, err
     }
     errorMapping := i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.ErrorMappings {
         "400": i2b7a3625368152c59661ed1a63c26960f7e9cda05d0fbc8e5d79ac57ca250e0a.CreateErrorFromDiscriminatorValue,
     }
-    res, err := m.BaseRequestBuilder.RequestAdapter.Send(ctx, requestInfo, CreateV2LocationItemWithUuResponseFromDiscriminatorValue, errorMapping)
+    res, err := m.BaseRequestBuilder.RequestAdapter.SendPrimitive(ctx, requestInfo, "[]byte", errorMapping)
     if err != nil {
         return nil, err
     }
     if res == nil {
         return nil, nil
     }
-    return res.(V2LocationItemWithUuResponseable), nil
-}
-// PostAsWithUuPostResponse move files to the specified location.
-// returns a V2LocationItemWithUuPostResponseable when successful
-// returns a Error error when the service returns a 400 status code
-func (m *V2LocationWithUuItemRequestBuilder) PostAsWithUuPostResponse(ctx context.Context, requestConfiguration *V2LocationWithUuItemRequestBuilderPostRequestConfiguration)(V2LocationItemWithUuPostResponseable, error) {
-    requestInfo, err := m.ToPostRequestInformation(ctx, requestConfiguration);
-    if err != nil {
-        return nil, err
-    }
-    errorMapping := i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.ErrorMappings {
-        "400": i2b7a3625368152c59661ed1a63c26960f7e9cda05d0fbc8e5d79ac57ca250e0a.CreateErrorFromDiscriminatorValue,
-    }
-    res, err := m.BaseRequestBuilder.RequestAdapter.Send(ctx, requestInfo, CreateV2LocationItemWithUuPostResponseFromDiscriminatorValue, errorMapping)
-    if err != nil {
-        return nil, err
-    }
-    if res == nil {
-        return nil, nil
-    }
-    return res.(V2LocationItemWithUuPostResponseable), nil
+    return res.([]byte), nil
 }
 // returns a *RequestInformation when successful
 func (m *V2LocationWithUuItemRequestBuilder) ToGetRequestInformation(ctx context.Context, requestConfiguration *V2LocationWithUuItemRequestBuilderGetRequestConfiguration)(*i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.RequestInformation, error) {
@@ -109,13 +88,17 @@ func (m *V2LocationWithUuItemRequestBuilder) ToGetRequestInformation(ctx context
 }
 // ToPostRequestInformation move files to the specified location.
 // returns a *RequestInformation when successful
-func (m *V2LocationWithUuItemRequestBuilder) ToPostRequestInformation(ctx context.Context, requestConfiguration *V2LocationWithUuItemRequestBuilderPostRequestConfiguration)(*i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.RequestInformation, error) {
+func (m *V2LocationWithUuItemRequestBuilder) ToPostRequestInformation(ctx context.Context, body i2b7a3625368152c59661ed1a63c26960f7e9cda05d0fbc8e5d79ac57ca250e0a.MoveRequestable, requestConfiguration *V2LocationWithUuItemRequestBuilderPostRequestConfiguration)(*i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.RequestInformation, error) {
     requestInfo := i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.NewRequestInformationWithMethodAndUrlTemplateAndPathParameters(i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.POST, m.BaseRequestBuilder.UrlTemplate, m.BaseRequestBuilder.PathParameters)
     if requestConfiguration != nil {
         requestInfo.Headers.AddAll(requestConfiguration.Headers)
         requestInfo.AddRequestOptions(requestConfiguration.Options)
     }
     requestInfo.Headers.TryAdd("Accept", "application/json")
+    err := requestInfo.SetContentFromParsable(ctx, m.BaseRequestBuilder.RequestAdapter, "application/json", body)
+    if err != nil {
+        return nil, err
+    }
     return requestInfo, nil
 }
 // WithUrl returns a request builder with the provided arbitrary URL. Using this method means any other path or query parameters are ignored.
